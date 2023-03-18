d6c45bca3561f0c8eb23321922bab07fec6400f0
leonardo oliveira <leonardo_olivesilva@icloud.com>
2023-03-18 13:37:03 -0700

    BankEncapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDep = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDep);
+            Console.Write($"Thank you for you deposit! Your balance is now {account.GetBalance()}");
         }
     }
 }
